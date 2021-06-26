69a591085b6630167f1f4861631323f593168219
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-06-26 18:14:46 +0530

    remove unnecessary folders and imports

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,6 @@
-import 'package:blog/core/services/api.dart';
 import 'package:blog/core/viewmodels/edit_model.dart';
 import 'package:get_it/get_it.dart';
 
-import 'core/services/authentication.dart';
 import 'core/viewmodels/home_model.dart';
 
 GetIt locator = GetIt.instance;
