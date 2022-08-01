59d0dd8a286c7eb7847c4737f0ba589ef85d583a
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2022-08-01 19:16:30 +0530

    Fix deprecated body1 in TextTheme

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
- @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Pixelate",
       theme: ThemeData(
-        fontFamily: "Retro",
-        iconTheme: IconThemeData(
-          color: Colors.white,
-        ),
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.white)
-        ),
-        bottomSheetTheme: BottomSheetThemeData(
-          backgroundColor: Colors.black.withOpacity(0),
-        )
-      ),
+          fontFamily: "Retro",
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+          textTheme: TextTheme(bodyText1: TextStyle(color: Colors.white)),
+          bottomSheetTheme: BottomSheetThemeData(
+            backgroundColor: Colors.black.withOpacity(0),
+          )),
       initialRoute: '/',
       onGenerateRoute: CustomRouter.generateRoute,
     );
